94913b09671cda920e69a719536f8a0215525bc0
Peter Xiao <peterxiaofun@gmail.com>
2024-02-11 10:29:55 -0500

    removed previous visbility switch as modules do it

--- build/page.js
+++ build/page.js
@@ -1,12 +1,3 @@
-// set up page visibility by clicking on section links
-function showPage(pageId) {
-    var pages = document.querySelectorAll('.page');
-    for (var i = 0; i < pages.length; i++) {
-        pages[i].classList.remove('active');
-    }
-    document.getElementById(pageId).classList.add('active');
-}
-
 // get modularized html file
 function loadPage(pageURL) {
     fetch(pageURL)
